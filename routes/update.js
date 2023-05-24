36639f7d93812fc02ab646bf10ee2d7cd0de7571
Shahid Raza <shahidraza02qwert@gmail.com>
2023-05-24 17:45:25 +0530

    add sign up page

--- routes/update.js
+++ routes/update.js
@@ -2,7 +2,7 @@ const route=require('express').Router();
 const controllers=require('../controllers/employees'); 
 
 
-route.get('/signup',controllers.sign_up);
+route.get('/',controllers.sign_up);
 route.post('/signup',controllers.signup_post);
 
 module.exports=route;
