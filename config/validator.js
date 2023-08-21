a0918ed036469a0caeffc4ed6759937d15da4c0b
Shahid Raza <shahidraza02qwert@gmail.com>
2023-08-21 20:03:09 +0530

    modify validator

--- config/validator.js
+++ config/validator.js
@@ -3,7 +3,7 @@
 const joi=require('@hapi/joi');
 module.exports.registrationValidator=(data)=>{
       const schema= joi.object({
-       name:joi.string().alphanum().min(3).max(30).required(),
+       name:joi.string().string().min(3).max(30).required(),
        email:joi.string().email().min(6).max(40).required(),
        password:joi.string().min(6).max(20).required(),
        confirmpassword:joi.string().min(6).max(20).required()
